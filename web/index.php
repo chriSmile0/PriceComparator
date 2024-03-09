5f22b7042de7b20f8594a4bf5533a2b1585542eb
chriSmile0 <crspitz@outlook.fr>
2024-03-09 12:36:24 +0100

    web app setup

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="chriSmile0">
+    <meta name="description" content="Mimi Recipes">
+    <meta charset="UTF-8">
+    <link href="style_home.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" 
+          rel="stylesheet">
+	<link rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>PriceComparator</title>
+</head>
+<body>
+	<header>
+		<h1>PriceComparator</h1>
+	</header>
+	<script src="common.js"></script>
+</body>
+</html>
